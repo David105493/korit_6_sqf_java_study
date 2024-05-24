348fd2a31cadbf27e8fa11c5d231838d36eb7fcf
David105493 <sderchaos@naver.com>
2024-05-24 17:53:24 +0900

    자바기초수업524

--- src/main/java/com/study/java_study/ch04_제어/Star.java
+++ src/main/java/com/study/java_study/ch04_제어/Star.java
@@ -3,7 +3,7 @@ package com.study.java_study.ch04_제어;
 public class Star {
     public static void main(String[] args) {
         for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < (i+1); j++) {
+            for(int j = 0; j < (i + 1); j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -19,7 +19,7 @@ public class Star {
         System.out.println();
 
         for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5 - i; j++) {
+            for(int j = 0; j < 5 - 1 - i; j++) {
                 System.out.print(" ");
             }
             for(int k = 0; k < i + 1; k++) {
@@ -31,7 +31,7 @@ public class Star {
         System.out.println();
 
         for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < i + 1; j++) {
+            for(int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
             for(int k = 0; k < 5 - i; k++) {
@@ -42,10 +42,10 @@ public class Star {
         System.out.println();
 
         for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5 - i; j++) {
+            for(int j = 0; j < 5 - 1 - i; j++) {
                     System.out.print(" ");
             }
-            for(int k = 0; k < (i*2) + 1; k++) {
+            for(int k = 0; k < (i + 1) * 2 - 1; k++) {
                 System.out.print("*");
             }
 
